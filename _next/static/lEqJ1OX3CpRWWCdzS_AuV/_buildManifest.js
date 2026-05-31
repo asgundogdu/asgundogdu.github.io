2dc62e483d979d1f31ee861fcd8c709b3502577a
Ahmet Salih Gundogdu <gundogduasalih@gmail.com>
2026-05-31 12:00:19 -0400

    Publish static site from Pages branch root

---
+++ _next/static/lEqJ1OX3CpRWWCdzS_AuV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/302-3226bac6111223fd.js","static/chunks/pages/index-de4e12ae02b6efd6.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
